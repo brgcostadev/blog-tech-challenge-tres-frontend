759294ed70b71850d81ec88d8fc4c33544666c84
Bruno Costa <br9uno@gmail.com>
2026-09-12 12:58:24 -0300

    Cria estrutura inicial do frontend em React

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <main>
+      <h1>Posts</h1>
+
+      <input type="text" placeholder="Buscar posts..." />
+
+      <p>Nenhum post carregado ainda.</p>
+    </main>
+  );
+}
+
+export default Home;
